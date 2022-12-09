495a1df9d14a274fec4290ba82c8444c203ba30b
Raneem <aljamal.raneem@gmail.com>
2022-12-09 19:22:15 +0200

    Basic Queries

---
+++ sql-exercise-master/answers.sh
@@ -0,0 +1,17 @@
+﻿Basic Queries
+1. SELECT * FROM students;
+2. SELECT * FROM students
+    WHERE Age>”30”;
+3. SELECT * FROM students
+    WHERE Age="30"AND Gender="f";
+4. SELECT Points FROM students
+    WHERE Name="Alex";
+5. INSERT INTO students (Name, Age, Gender, Points)
+    VALUES ("Raneem", "37", "F", "400");
+6. UPDATE students
+    SET Points="350"
+    WHERE Name="Basma";
+7. UPDATE students
+   SET Points="150"
+   WHERE Name="Alex";
+8. 
